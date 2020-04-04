ddaa5e05674b9cc99a1540bd9d299a6316b081bc
Nam Phan <phannam1412@yahoo.com>
2020-04-04 10:55:15 +0700

    pwd

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # collect-docs-from-code
 Collect documentations from code and create a readme file
+
+Motivation
+==========
+
+I usually write a lots of explainatory comments in source code, but after finishing a project, 
+
+I should create a readme file for components, packages, modules that I have implemented. 
+
+At this time, I need to choose:
+
+- if I should copy all source code comments to the readme file and then delete these comments
+
+- or if I should write a new readme and still keep all source code comments
+
+- or not write a readme at all
+
+I really want to keep all source code comments because it must easier for anyone to read and understand source code quickly.
+
+So, my final solution is to write a tools to collect all source code comments and put everything into a readme file.
+
+How it work
+===========
+
+The script will parse source code files utilizing a sophisticated pattern matching algorithm 
+and create a dictionary for all items.
+
+Then it will sort all items and write to the destination file. 
+
+Reference
+=========
+
+- [Extended Backus–Naur form](https://en.wikipedia.org/wiki/Extended_Backus%E2%80%93Naur_form)
+
+- [Pattern matching implementation](https://github.com/phannam1412/go-pattern-matching)
---
+++ collect_docs_from_code.go
@@ -0,0 +1,155 @@
+package collect_docs_from_code
+
+import (
+	"fmt"
+	. "github.com/phannam1412/go-pattern-matching"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"sort"
+	"strings"
+)
+
+type Item struct {
+	Number string
+	Header string
+	Body string
+	Children []Item
+}
+
+func panicOnError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func getText(item *Item, level int, forIntro bool) string {
+	text := ""
+	if forIntro {
+		text += strings.Repeat("    ", level) + " " + item.Header + "\n\n"
+	} else {
+		text += strings.Repeat("#", level + 1) + " " + item.Header + "\n\n"
+		if item.Body != "" {
+			text += item.Body + "\n\n"
+		}
+	}
+	for _, child := range item.Children {
+		text += getText(&child, level + 1, forIntro)
+	}
+	return text
+}
+
+func Run(paths []string, findExtensions []string, writeResultTo string) {
+	parserFactory := func () func (text string) *Res {
+		numberWithDot := Combine(Number, Dot)
+		somethingBeforeComment := Any(Or(Whitespace, Tab))
+		startOfComment := Or(Sharp, Combine(Backsplash, Backsplash))
+		header := Combine(somethingBeforeComment, startOfComment, startOfComment, Whitespace, Combine(Some(numberWithDot), Whitespace, TextUntilLineEnd))
+		body := AndBut(Combine(somethingBeforeComment, startOfComment, TextUntilLineEnd), header)
+		multiline := Combine(header, Any(body))
+		formula := FullSearch(multiline, 0)
+		return func (text string) *Res {
+			tokens := Tokenize(text)
+			return formula(tokens, 0)
+		}
+	}
+	var items []*Item
+	resolver := func(res *Res) {
+		if res == nil {
+			println("no result")
+			return
+		}
+		for _, v := range res.Children {
+			number := v.Children[0].Children[4].Children[0].Value
+			header := strings.Trim(v.Children[0].Children[4].Value, " \n")
+			body := ""
+			for _, v2 := range v.Children[1].Children {
+				body += v2.Children[2].Value
+			}
+			body = strings.Trim(body, " \n")
+			items = append(items, &Item{
+				Number:   number,
+				Header:   header,
+				Body:     body,
+				Children: nil,
+			})
+		}
+	}
+	parser := parserFactory()
+
+	for _, v := range paths {
+		panicOnError(filepath.Walk(v, func(path string, info os.FileInfo, err error) error {
+			// ignore if this is directory, we only manipulate on file
+			if info.IsDir() {
+				return nil
+			}
+			name := info.Name()
+			isInvalidExtension := false
+			for _, findForExtension := range findExtensions {
+				if len(name) < len("." + findForExtension) {
+					continue
+				}
+				// cut last characters to see if it match with our expected extension
+				extension := name[len(name) - len(findForExtension) - 1:]
+				if extension != "." + findForExtension {
+					continue
+				}
+				isInvalidExtension = true
+				break
+			}
+			if isInvalidExtension == false {
+				return nil
+			}
+			fmt.Printf("===> extracting docs from %s...\n", path)
+			b, err := ioutil.ReadFile(path)
+			panicOnError(err)
+			res := parser(string(b))
+			resolver(res)
+			return nil
+		}))
+	}
+
+	// initialize a hash map for quick lookup Item by number 1.1., 2.2. etc...
+	fromNumberToItem := map[string]*Item{}
+	for _, v := range items {
+		fromNumberToItem[v.Number] = v
+	}
+
+	// distribute children to corresponding parent
+	for _, v := range items {
+		number := strings.Split(v.Number, ".")
+		// ignore if this item is at level 1
+		if len(number) < 3 {
+			continue
+		}
+		// get parent of this item, e.g. 4.2.1 has parent of 4.2.
+		parent := strings.Join(number[:len(number) - 2], ".") + "."
+		if _, ok := fromNumberToItem[parent]; !ok {
+			panic(fmt.Errorf("cannot find key %s", parent))
+		}
+		mparent := fromNumberToItem[parent]
+		mparent.Children = append(mparent.Children, *v)
+	}
+
+	// sort topics
+	sort.Slice(items, func(i, j int) bool {
+		return strings.Compare(items[i].Number, items[j].Number) <= 0
+	})
+
+	text := ""
+	intro := ""
+	// only start with items of level 1 and then recursively travel down to children
+	for _, v := range items {
+		number := strings.Split(v.Number, ".")
+		if len(number) >= 3 {
+			continue
+		}
+		intro += getText(v, 0, true)
+		text += getText(v, 0, false)
+	}
+	text = "**WARNING: GENERATED, DO NOT EDIT !!!**\n\n" +
+		intro +
+		"\n\n" +
+		text
+	panicOnError(ioutil.WriteFile(writeResultTo, []byte(text), 0777))
+}
